9992484d6673f0dac59b1ec4398166a34ae91c5e
tongsiyuan <tongsiyuan@corp.netease.com>
2022-01-04 18:21:21 +0800

    :recycle: refector home page UI

---
+++ docs/algorithm/模板/quickSort.md
@@ -0,0 +1,31 @@
+---
+id: quickSort
+title: 快速排序
+# hide_title: false
+# hide_table_of_contents: false
+# sidebar_label: Markdown :)
+# custom_edit_url: https://github.com/facebook/docusaurus/edit/master/docs/api-doc-markdown.md
+description: 算法基础
+keywords:
+  - quickSort
+  - C++
+  - frontend
+# image: https://i.imgur.com/mErPwqL.png
+---
+
+```C++
+void quick_sort(int ary[], int l, int r) {
+    if (l >= r) return;
+
+    int i = l - 1, j = r + 1, x = ary[l + r >> 1];
+
+    while(i < j) {
+        do i++; while(ary[i] < x);
+        do j--; while(ary[j] > x);
+        if (i < j) swap(ary[i], ary[j]);
+    }
+
+    quick_sort(ary, l, j);
+    quick_sort(ary, j + 1, r);
+}
+```
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
+import { useSpring, animated, useTrail } from "react-spring";
 import styled from "@emotion/styled";
-import BgImage from "@site/static/img/home/bg.svg";
+import BgImage from "@site/static/img/home/bg2.svg";
 import MusicUrl from "@site/static/img/home/music.svg";
 import Styles from "./HomepageFeatures.module.scss";
+import BillBill from "@site/static/img/home/billbill.svg";
+import QQ from "@site/static/img/home/qq.svg";
+import WeChat from "@site/static/img/home/weChat.svg";
 
 export default function HomepageFeatures() {
   const props = useSpring({
@@ -27,8 +30,27 @@ export default function HomepageFeatures() {
     delay: 600,
   });
 
+  const animatedTexts = useTrail(5, {
+    from: { opacity: 0, transform: "translateY(3em)" },
+    to: { opacity: 1, transform: "translateY(0)" },
+    config: {
+      mass: 3,
+      friction: 45,
+      tension: 460,
+    },
+  });
+
   return (
     <div className={Styles.mainWrapper}>
+      <div style={props} className={Styles.title}>
+        <div>
+          Welcome to
+          <span>JiuRan</span>
+          's Channel
+        </div>
+        <div class={Styles.subTitle}>计算机编程爱好者, 深耕前端</div>
+        {/* <SocialLinks animatedProps={animatedTexts[4]} /> */}
+      </div>
       <div
         style={{
           display: "flex",
@@ -38,26 +60,27 @@ export default function HomepageFeatures() {
       >
         <SvgBackGround />
       </div>
-      <div style={props} className={Styles.title}>
-        Welcome to
-        <span>JiuRan</span>
-        's Channel
-      </div>
-      <Music></Music>
+      {/* <Music></Music> */}
     </div>
   );
 }
 
-const WelcomeTitle = styled(animated.h1)`
-  margin-bottom: 50px;
-  flex: 1 1;
-  display: flex;
-  font-size: 3rem;
-  justify-content: center;
-  & > span {
-    color: #338bff;
-  }
-`;
+function SocialLinks({ animatedProps, ...props }) {
+  // const { isDarkTheme } = useThemeContext();
+  return (
+    <animated.div className={Styles.socialLinks} style={animatedProps}>
+      <a href="https://space.bilibili.com/345132581">
+        <BillBill />
+      </a>
+      <a href="https://space.bilibili.com/345132581">
+        <QQ />
+      </a>
+      <a href="https://space.bilibili.com/345132581">
+        <WeChat />
+      </a>
+    </animated.div>
+  );
+}
 
 const SvgBackGround = styled(BgImage)`
   height: 70%;
@@ -66,8 +89,6 @@ const SvgBackGround = styled(BgImage)`
   background-position: right center;
   background-size: cover;
 `;
-
-const MainWrapper = styled.div``;
 
 const Music = styled(MusicUrl)`
   position: fixed;
